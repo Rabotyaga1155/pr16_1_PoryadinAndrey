f84e0d3eccc283803531883806edbb77c9bb9295
Rabotyaga1155 <123551706+Rabotyaga1155@users.noreply.github.com>
2023-05-05 10:23:52 +0500

    Add files via upload

---
+++ prakt16/prakt16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+namespace prakt16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "text.txt";
+            string text = File.ReadAllText(path).ToLower();
+            string[] myArray = text.Split(" ");
+            Console.WriteLine("Введите слово которое хотите искать");
+            string p = Console.ReadLine().ToLower();
+            int count = myArray.Count();
+            Console.WriteLine("Слов в тексте " +count);
+            count = myArray.Count(s => s.StartsWith(p));
+            Console.WriteLine("Количество слов - " + myArray.Count(item => item == p));
+        }
+    }
+}
---
+++ prakt16_2/prakt16_2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace prakt16_2
+{
+    public partial class Form1 : Form
+    {
+        List<string> list = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list.Add(richTextBox1.Text);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string vvod = richTextBox1.Text;
+            string[] array = vvod.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int digitCount = array.SelectMany(s => s).Count(char.IsDigit);
+            richTextBox2.AppendText($"Цифр в массиве: {digitCount}\n\n");
+            richTextBox2.AppendText("Элементы массива до символа \"/\":\n");
+            foreach (string s in array.TakeWhile(s => s != "/"))
+            {
+                richTextBox2.AppendText(s + "\n");
+            }
+            richTextBox2.AppendText("\n");
+            richTextBox2.AppendText("Элементы массива после символа \"/\" :\n");
+            string[] newArray = array.SkipWhile(s => s != "/").Skip(1).Select(s => s.ToUpper() == s ? s.ToLower() : s.ToUpper()).ToArray();
+            foreach (string s in newArray)
+            {
+                richTextBox2.AppendText(s + "\n");
+            }
+            File.WriteAllLines("info.txt", newArray);
+           
+        }
+    }
+}
